9fcd580f0a60b4ca0737a525bfe2658b907ab278
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2020-10-23 13:04:12 +0200

    update

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,16 +1,14 @@
 const arr = [5, 12, 1, 4, 56, 21, 30, 9, 19, 3, 2];
 
-for (let i = 1; i < arr.length; i ++) {
-	let idx;
-	for (let j = i - 1; j >= 0; j--) {
-		if (arr[i] < arr[j]) {
-			idx = j;
-		}
+for (let i = 1; i < arr.length; ++i) {
+	const key = arr[i];
+	let j = i - 1;
+
+	while (j >= 0 && arr[j] > key) {
+		arr[j + 1] = arr[j];
+		j = j - 1;
 	}
-	if (idx) {
-		const removed = arr.splice(arr[i], 1)[0];
-		arr.splice(idx, 0, arr[idx]);
-	}
+	arr[j + 1] = key;
 }
 
 console.log(arr);
