13d23e6ce3657abe60ffc8bc54364bf5a1f28fe3
Milan Timotijevic <milan.timotijevic@basiq.io>
2024-01-06 20:51:26 +0100

    update fibo example

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-
+// iterative
 let first = 0;
 let second = 1;
 let third;
@@ -14,3 +14,15 @@ for (let i = 0; i < 100; i++) {
 	console.log(third)
 }
 
+//recursive
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(fibonacci(i));
+}
